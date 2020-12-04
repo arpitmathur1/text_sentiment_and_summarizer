4385ce561e8321b02e756dd882861c265fda6274
Arpit Mathur <20039537+arpitmathur1@users.noreply.github.com>
2020-12-05 00:54:21 +1100

    basic setup, csv file downloaded

--- library_based.py
+++ library_based.py
@@ -2,5 +2,38 @@
 """
 Sentiment analysis
 Summarize
+
+data: https://data.world/crowdflower/sentiment-analysis-in-text
 """
 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+analyser = SentimentIntensityAnalyzer()
+LANGUAGE = "english"
+SENTENCES_COUNT = 4
+
+def sentiment_analyzer_scores(sentence):
+    score = analyser.polarity_scores(sentence)
+    print("{:-<40} {}".format(sentence, str(score)))
+
+sentiment_analyzer_scores("The phone is super cool.")
+
+
+url = "https://en.wikipedia.org/wiki/Automatic_summarization"
+parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+# or for plain text files
+# parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
+stemmer = Stemmer(LANGUAGE)
+
+summarizer = Summarizer(stemmer)
+summarizer.stop_words = get_stop_words(LANGUAGE)
+
+for sentence in summarizer(parser.document, SENTENCES_COUNT):
+    print(sentence)
