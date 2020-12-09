15ef17c557626c3c587f5982faa6ae0428de7eb1
Arpit Mathur <20039537+arpitmathur1@users.noreply.github.com>
2020-12-09 22:07:14 +1100

    Update summarize_LSTM.py

--- summarize_LSTM.py
+++ summarize_LSTM.py
@@ -6,3 +6,12 @@ import random
 random.seed(0)
 
 
+train = pd.read_csv("news_summary_more.csv")
+print("before removing duplicates", len(train))
+
+# drop some duplicates
+train = train.drop_duplicates(subset='text', keep="last")
+
+print("after removing duplicates", len(train))
+train = train.reset_index(drop=True)
+print(train.head())
